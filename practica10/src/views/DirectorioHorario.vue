d3b992e4cd4af0c8c5b0ed526859e2724d9d4d29
geramos2004-codificador <geramos.2004@gmail.com>
2025-10-23 13:54:27 -0600

    directorio y horario

---
+++ practica10/src/views/DirectorioHorario.vue
@@ -0,0 +1,80 @@
+<template>
+  <div >
+    
+    <!-- Imagen superior del encabezado -->
+    <img 
+      src="../assets/directorio.jpg" 
+      alt="Encabezado Directorio" 
+      class="encabezado"
+    />
+
+    <!-- Contenido central -->
+    <div class="contenido">
+      
+      <!-- Título -->
+      <h1>
+        Directorio
+      </h1>
+
+      <!-- Información de contacto -->
+      <div >
+        <p >
+          Ministra de Educación, Ciencia y Tecnología
+        </p>
+        <p >Karla Edith Trigueros</p>
+        <p >
+          Alameda Juan Pablo II y calle Guadalupe, Centro de Gobierno, Plan Maestro, 
+          edificio A-2, tercer nivel, San Salvador, El Salvador.
+        </p>
+      </div>
+    </div>
+
+    <!-- Imagen inferior de las 5 opciones -->
+    
+
+
+
+    <img 
+      src="../assets/horarios.png" 
+      alt="Opciones Directorio" 
+      class="horario"
+    />
+
+
+    
+ <!-- Contenido central -->
+    <div class="contenido">
+      
+      <!-- Título -->
+      <h1>
+         Horario de Atencion
+      </h1>
+
+      <!-- Información de contacto -->
+      <div >
+        <p >
+         Nuestros horarios de atención:
+        </p>
+        <p >De Lunes a Viernes de 7:30 a.m. a 3:30 p.m.</p>
+        
+      </div>
+    </div>
+
+<img 
+      src="../assets/cap.png" 
+      alt="Opciones Directorio" 
+      class="inferior"
+    />
+
+  </div>
+
+
+
+  
+</template>
+
+<script>
+export default {
+  name: "DirectorioHorario",
+};
+</script>
